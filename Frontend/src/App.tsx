75c2bec9f0008322603df2e8116e14fe20f4a182
UltimateGG <clubblock1@gmail.com>
2024-01-27 13:41:37 -0600

    Add tailwind css, base styles

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,32 +1,21 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
 
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1>IP Whitelist Tracker</h1>
+
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
