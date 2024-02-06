5aaa5e6e055b27fe505c24c4aa00da467754e515
UltimateGG <clubblock1@gmail.com>
2024-02-05 19:47:43 -0600

    Base css again

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,26 +1,24 @@
 import { useState } from 'react';
-import SmellButton from './smell.tsx';
+import SmellButton from './smell';
 
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <SmellButton count={count}></SmellButton>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1>IP Whitelist Tracker</h1>
+
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+
+      <SmellButton count={count} />
+
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
-    </>
+    </div>
   );
 };
 
