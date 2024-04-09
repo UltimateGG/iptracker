96bb1d9fcaeb72f25221c75168f009e36bddae03
MilesL35 <96485567+MilesL35@users.noreply.github.com>
2024-04-08 19:26:29 -0500

    Fixed Formatting on HomePage.tsx
    My VS Code instance was reformatting the file to keep everything under 120 characters long butchering the file. This fixes that.

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,69 +1,35 @@
 import { useAppContext } from "../contexts/AppContext";
 import { useAuth } from "../contexts/AuthContext";
 import { Button, Dropdown, Spinner, TextInput } from "flowbite-react";
-import {
-  Application,
-  SortDirection,
-  SortState,
-  SortType,
-  UserRole,
-} from "../utils/types";
+import { Application, SortDirection, SortState, SortType, UserRole } from "../utils/types";
 import { HiChevronDown, HiPlus, HiSearch, HiServer } from "react-icons/hi";
 import CreateApplicationModal from "./admin/CreateApplicationModal";
 import { useMemo, useState } from "react";
 import ApplicationEntry from "../components/ApplicationEntry";
-import {
-  FaRegWindowMaximize,
-  FaSortAmountDown,
-  FaSortAmountUp,
-} from "react-icons/fa";
+import { FaRegWindowMaximize, FaSortAmountDown, FaSortAmountUp } from "react-icons/fa";
 import clsx from "clsx";
 
 const HomePage = () => {
-  const [createApplicationModalOpen, setCreateApplicationModalOpen] =
-    useState(false);
+  const [createApplicationModalOpen, setCreateApplicationModalOpen] = useState(false);
   const [search, setSearch] = useState("");
-  const [sort, setSort] = useState<SortState>({
-    direction: SortDirection.DESC,
-    type: SortType.APPLICATION_ID,
-  });
+  const [sort, setSort] = useState<SortState>({ direction: SortDirection.DESC, type: SortType.APPLICATION_ID });
 
   const { user } = useAuth();
   const { applications } = useAppContext();
 
-  const serverCount = useMemo(
-    () => applications?.reduce((acc, app) => acc + app.servers.length, 0),
-    [applications]
-  );
+  const serverCount = useMemo(() => applications?.reduce((acc, app) => acc + app.servers.length, 0), [applications]);
   const sortedApplications = useMemo(() => {
     if (!applications) return null;
 
     function filterApplications(app: Application) {
-      return (
-        app.description.toLowerCase().includes(search.toLowerCase()) ||
-        app.servers.filter((server) => server.sourceIpAddress.includes(search))
-          .length != 0 ||
-        app.servers.filter((server) =>
-          server.sourceHostname.toLowerCase().includes(search.toLowerCase())
-        ).length != 0 ||
-        app.servers.filter((server) =>
-          server.destinationIpAddress.includes(search)
-        ).length != 0 ||
-        app.servers.filter((server) =>
-          server.destinationHostname
-            .toLowerCase()
-            .includes(search.toLowerCase())
-        ).length != 0 ||
-        app.servers.filter((server) =>
-          server.destinationPort.toString().includes(search)
-        ).length != 0 ||
-        app.servers.filter(
-          (server) => server.enabled && search.toLowerCase().includes("yes")
-        ).length != 0 ||
-        app.servers.filter(
-          (server) => !server.enabled && search.toLowerCase().includes("no")
-        ).length != 0
-      );
+      return app.description.toLowerCase().includes(search.toLowerCase()) || 
+      app.servers.filter((server) => server.sourceIpAddress.includes(search)).length != 0 || 
+      app.servers.filter((server) => server.sourceHostname.toLowerCase().includes(search.toLowerCase())).length != 0 || 
+      app.servers.filter((server) => server.destinationIpAddress.includes(search)).length != 0 || 
+      app.servers.filter((server) => server.destinationHostname.toLowerCase().includes(search.toLowerCase())).length != 0 || 
+      app.servers.filter((server) => server.destinationPort.toString().includes(search)).length != 0 || 
+      app.servers.filter((server) => server.enabled && search.toLowerCase().includes("yes")).length != 0 || 
+      app.servers.filter((server) => !server.enabled && search.toLowerCase().includes("no")).length != 0;
     }
 
     const filtered = applications.filter(filterApplications);
@@ -74,14 +40,10 @@ const HomePage = () => {
         sorted = filtered.sort((a, b) => a.id - b.id);
         break;
       case SortType.CREATED_AT:
-        sorted = filtered.sort(
-          (a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt)
-        );
+        sorted = filtered.sort((a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt));
         break;
       case SortType.MODIFIED_AT:
-        sorted = filtered.sort(
-          (a, b) => Date.parse(a.modifiedAt) - Date.parse(b.modifiedAt)
-        );
+        sorted = filtered.sort((a, b) => Date.parse(a.modifiedAt) - Date.parse(b.modifiedAt));
         break;
       case SortType.SERVER_COUNT:
         sorted = filtered.sort((a, b) => a.servers.length - b.servers.length);
@@ -96,16 +58,10 @@ const HomePage = () => {
       {user && (
         <>
           <div className="flex justify-between w-full flex-col sm:flex-row">
-            <h2 className="text-2xl font-medium">
-              Welcome back, {user.username}!
-            </h2>
+            <h2 className="text-2xl font-medium">Welcome back, {user.username}!</h2>
 
             {user.role === UserRole.ADMIN && (
-              <Button
-                size="sm"
-                className="h-min mt-4 sm:mt-0 whitespace-nowrap"
-                onClick={() => setCreateApplicationModalOpen(true)}
-              >
+              <Button size="sm" className="h-min mt-4 sm:mt-0 whitespace-nowrap" onClick={() => setCreateApplicationModalOpen(true)}>
                 <HiPlus size={20} className="mr-2" />
                 New Application
               </Button>
@@ -134,52 +90,20 @@ const HomePage = () => {
             <div className="mb-4" />
           )}
 
-          <h3 className="text-xl font-medium mb-2">
-            {user.role === UserRole.ADMIN ? "All" : "Your"} Applications
-          </h3>
+          <h3 className="text-xl font-medium mb-2">{user.role === UserRole.ADMIN ? "All" : "Your"} Applications</h3>
           <div className="flex gap-4 flex-col sm:flex-row">
-            <TextInput
-              placeholder="Search applications, server hostname, ip address..."
-              icon={HiSearch}
-              className="w-full sm:mb-6"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
+            <TextInput placeholder="Search applications, server hostname, ip address..." icon={HiSearch} className="w-full sm:mb-6" value={search} onChange={(e) => setSearch(e.target.value)} />
 
             <div className="flex mb-6">
-              <Button
-                color="light"
-                className="h-[42px] rounded-r-none"
-                title={
-                  sort.direction === SortDirection.ASC
-                    ? "Ascending"
-                    : "Descending"
-                }
-                onClick={() =>
-                  setSort((s) => ({
-                    ...s,
-                    direction:
-                      sort.direction === SortDirection.ASC
-                        ? SortDirection.DESC
-                        : SortDirection.ASC,
-                  }))
-                }
-              >
-                {sort.direction === SortDirection.ASC ? (
-                  <FaSortAmountUp size={20} />
-                ) : (
-                  <FaSortAmountDown size={20} />
-                )}
+              <Button color="light" className="h-[42px] rounded-r-none" title={sort.direction === SortDirection.ASC ? "Ascending" : "Descending"} onClick={() => setSort((s) => ({ ...s, direction: sort.direction === SortDirection.ASC ? SortDirection.DESC : SortDirection.ASC }))}>
+                {sort.direction === SortDirection.ASC ? <FaSortAmountUp size={20} /> : <FaSortAmountDown size={20} />}
               </Button>
 
               <Dropdown
                 label=""
                 renderTrigger={() => (
                   <div className="h-[42px]">
-                    <Button
-                      color="light"
-                      className="flex items-center justify-between !border-l-0 !rounded-l-none whitespace-nowrap"
-                    >
+                    <Button color="light" className="flex items-center justify-between !border-l-0 !rounded-l-none whitespace-nowrap">
                       {sort.type}
                       <HiChevronDown size={20} className="ml-2" />
                     </Button>
@@ -187,13 +111,7 @@ const HomePage = () => {
                 )}
               >
                 {Object.values(SortType).map((v) => (
-                  <Dropdown.Item
-                    key={v}
-                    onClick={() => setSort((s) => ({ ...s, type: v }))}
-                    className={clsx(
-                      sort.type === v && "bg-gray-200 dark:bg-gray-500"
-                    )}
-                  >
+                  <Dropdown.Item key={v} onClick={() => setSort((s) => ({ ...s, type: v }))} className={clsx(sort.type === v && "bg-gray-200 dark:bg-gray-500")}>
                     {v}
                   </Dropdown.Item>
                 ))}
@@ -213,11 +131,7 @@ const HomePage = () => {
         </>
       )}
 
-      {createApplicationModalOpen && (
-        <CreateApplicationModal
-          onClose={() => setCreateApplicationModalOpen(false)}
-        />
-      )}
+      {createApplicationModalOpen && <CreateApplicationModal onClose={() => setCreateApplicationModalOpen(false)} />}
     </div>
   );
 };
